b58e437d6f94b8280aebd1259a2d9892f8204f59
devireddyprasanth22 <dpkrdrkr@gmail.com>
2025-03-23 15:08:08 +0800

    update to export as library for build

--- build/CMakeFiles/test-submodule32.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test-submodule32.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dp/Desktop/speqtral_challenge/master-software/submodule-3/submodule-3.2
-# Build directory: /Users/dp/Desktop/speqtral_challenge/master-software/submodule-3/submodule-3.2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(submodule32 "/Users/dp/Desktop/speqtral_challenge/master-software/submodule-3/submodule-3.2/build/test-submodule32")
-set_tests_properties(submodule32 PROPERTIES  _BACKTRACE_TRIPLES "/Users/dp/Desktop/speqtral_challenge/master-software/submodule-3/submodule-3.2/CMakeLists.txt;18;add_test;/Users/dp/Desktop/speqtral_challenge/master-software/submodule-3/submodule-3.2/CMakeLists.txt;0;")
